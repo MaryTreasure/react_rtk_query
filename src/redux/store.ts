d6d36984c2499079dce9e837c2dc87fe6e252bd2
MaryTreasure <mari199191@mail.ru>
2024-05-07 22:04:25 +0300

    feat: created store

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { goodsApi } from './goodsApi'
+
+export const store = configureStore({
+  reducer: {
+    [goodsApi.reducerPath]: goodsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(goodsApi.middleware),
+})
+
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
